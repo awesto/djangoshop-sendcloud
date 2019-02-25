9b149516b37075358401cd141d6ad6e4305fd6cb
Jacob Rief <jacob.rief@gmail.com>
2019-02-25 21:35:23 +0100

    remove django-shop dependency

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
         'requests',
         'django-phonenumber-field',
         'phonenumbers',
-        'django-shop>=1.0,<1.1'
     ],
     packages=find_packages(),
     include_package_data=True,
